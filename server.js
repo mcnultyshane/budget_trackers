f1a9871b7f13bfafb4f4d672fc36fe9069469251
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-07-29 23:25:47 -0400

    added MONGODB_URI

--- server.js
+++ server.js
@@ -15,9 +15,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
 });
 
 // routes
